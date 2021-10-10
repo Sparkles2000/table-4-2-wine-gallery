8f10f9767a9c2fb9e98a415e855452d8bb979e87
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-10 15:40:28 -0500

    Added relations to Customergroup model.

--- app/models/customergroup.rb
+++ app/models/customergroup.rb
@@ -1,2 +1,12 @@
 class Customergroup < ApplicationRecord
+    has_many :artsessions, dependent: :destroy
+    has_many :artpieces, through: :artsessions
+    has_many :winepurchases, dependent: :destroy
+    has_many :brandofwines, dependent: :destroy
+    has_many :brandofwines, through: :winepurchases
+    
+    accepts_nested_attributes_for :artpieces
+    accepts_nested_attributes_for :artsessions
+    accepts_nested_attributes_for :brandofwines
+    accepts_nested_attributes_for :winepurchases
 end
