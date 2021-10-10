5ed5fab8d32bd4fde0976266e514528f08d1f65a
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-10 14:37:46 -0500

    Created migration table Brand Of Wines using rails g resource.

---
+++ db/migrate/20211010193554_create_brandofwines.rb
@@ -0,0 +1,15 @@
+class CreateBrandofwines < ActiveRecord::Migration[6.1]
+  def change
+    create_table :brandofwines do |t|
+      t.string :brand
+      t.iteger :age
+      t.string :winetype
+      t.string :price
+      t.string :drysweet
+      t.string :alcoholcontent
+      t.string :img_src
+
+      t.timestamps
+    end
+  end
+end
