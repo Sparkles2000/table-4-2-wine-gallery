b0ce0cd13f9d1ca35b42944ada4d0224f738678e
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-10 16:34:37 -0500

    Added nesting to Art Session.

--- app/serializers/artsession_serializer.rb
+++ app/serializers/artsession_serializer.rb
@@ -1,3 +1,5 @@
 class ArtsessionSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :artstyle, :price, :sessiontype, :piecespergroup
+  belongs_to :customergroup
+  has_many :artpieces
 end
